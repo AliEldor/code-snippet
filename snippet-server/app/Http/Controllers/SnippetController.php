c63152b6c2d8c11d80517b277c255421c02387ec
Ali Eldor <alieldor02@gmail.com>
2025-03-24 01:03:09 +0200

    get a specific snippet

--- snippet-server/app/Http/Controllers/SnippetController.php
+++ snippet-server/app/Http/Controllers/SnippetController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Snippet;
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SnippetController extends Controller
 {
-    //
+    public function getSnippets()
+    {
+        $snippets = Snippet::with('tags')->where('user_id', Auth::id())->get();
+        
+        return response()->json([
+            "success" => true,
+            "snippets" => $snippets,
+        ]);
+    }
 }

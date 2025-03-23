00572e8132c3b54ad42cbc4661866dda2403b845
Ali Eldor <alieldor02@gmail.com>
2025-03-24 01:31:30 +0200

    addOrUpdate

--- snippet-server/app/Http/Controllers/SnippetController.php
+++ snippet-server/app/Http/Controllers/SnippetController.php
@@ -37,5 +37,57 @@ class SnippetController extends Controller
             "snippet" => $snippet,
         ]);
     }
+
+
+    public function addOrUpdateSnippet(Request $request, $id = "add"){
+        if($id=="add"){
+            $snippet = new Snippet();
+            $snippet->user_id = Auth::id();
+            $operation = "created";
+        }
+        else{
+            $snippet = Snippet::find($id);
+
+            // check if snippet exists for a user
+            if (!$snippet || $snippet->user_id != Auth::id()) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "Unable to find snippet",
+                    "snippet" => null
+                ], 404);
+            }
+            $operation = "updated";
+        }
+
+        $snippet->title = $request["title"];
+        $snippet->code = $request["code"];
+        $snippet->language = $request["language"];
+        $snippet->is_favorite = $request["is_favorite"] ?? false;
+        $snippet->save();
+
+        if ($request->has('tags') && !empty($request->tags)) {
+            // comma between tags
+            $tagsList = array_filter(array_map('trim', explode(',', $request->tags)));
+            
+            
+            $tagIds = [];
+            foreach ($tagsList as $tag) {
+                $tagRecord = Tag::firstOrCreate(['name' => $tag]);
+                $tagIds[] = $tagRecord->id;
+            }
+            
+            // Sync tags with snippet
+            $snippet->tags()->sync($tagIds);
+        }
+
+        $snippet = Snippet::with('tags')->find($snippet->id);
     
+    
+    return response()->json([
+        "success" => true,
+        "message" => "Snippet {$operation} successfully",
+        "snippet" => $snippet
+    ]);
+
+    }
 }

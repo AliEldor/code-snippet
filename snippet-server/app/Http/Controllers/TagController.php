831020e29de43a07b6d0ff186fcf1a85f45516c6
Ali Eldor <alieldor02@gmail.com>
2025-03-24 03:23:02 +0200

    getTags

--- snippet-server/app/Http/Controllers/TagController.php
+++ snippet-server/app/Http/Controllers/TagController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
 {
-    //
+    public function getTags()
+    {
+        $tags = Tag::whereHas('snippets', function($query) {
+            $query->where('user_id', Auth::id());
+        })->get();
+        
+        return response()->json([
+            'success' => true,
+            'tags' => $tags
+        ]);
+    }
 }
